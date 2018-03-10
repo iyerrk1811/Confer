ad42ea45976ed7669ba24870b45456fa8c57fc1a
Radhakrishnan <iyerrk1811@gmail.com>
2018-03-10 14:43:14 +1100

    Commit and push to CodeBranch1

---
+++ app/src/main/java/confer/app/confer/ComposeMailActivity.java
@@ -0,0 +1,32 @@
+package confer.app.confer;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ComposeMailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_mail);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).
+                // setAction("Action", null).show();
+                goToParticipant();
+            }
+        });
+    }
+
+    void goToParticipant() {
+        Intent intent = new Intent(this, Participant.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/confer/app/confer/Participant.java
+++ app/src/main/java/confer/app/confer/Participant.java
@@ -1,11 +1,18 @@
 package confer.app.confer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class Participant extends AppCompatActivity {
 
@@ -20,10 +27,54 @@ public class Participant extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).
+                // setAction("Action", null).show();
+                goToComposeMail();
             }
         });
     }
 
+    void goToComposeMail() {
+        Intent intent = new Intent(this, ComposeMailActivity.class);
+        startActivity(intent);
+    }
+
+    //RecyclerView Code for recyclerview present in participant class
+    /*
+    private List<TextView> items;
+    private int itemLayout;
+
+    public Participant(List<TextView> items, int itemLayout){
+        this.items = items;
+        this.itemLayout = itemLayout;
+    }
+
+    //@Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(itemLayout, parent, false);
+        return new ViewHolder(v);
+        //return null;
+    }
+
+    //@Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        TextView item = items.get(position);
+        holder.text.setText(item.getText());
+    }
+
+    //@Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        //public ImageView image;
+        public TextView text;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            text = (TextView) itemView.findViewById(R.id.text);
+        }
+    }
+    */
 }
